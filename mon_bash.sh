f1e8fd949727c83fdbf35514a7afbe58e1ac3204
El Hadji Cheikh Abdoulaye DIOP <elhadjicheikh.diop@uadb.edu.sn>
2026-09-01 03:25:04 +0000

    Ajoute mon_bash.sh

---
+++ mon_bash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo -e "
+1- se connecter au noeud principal d'un AWS ParallelCluster ;
+2- naviguer dans Linux et transferer des fichiers ;
+3- cloner et examiner un projet open source sans executer aveuglement son code ;
+4- expliquer Git, GitHub, depot, commit, branche, clone, pull et push ;
+5- installer EMOD dans un environnement Python isole ;
+6- demander des ressources et lancer un job avec Slurm ;
+7- lancer, surveiller et documenter une simulation de paludisme ;
+8- recuperer les resultats et publier un compte rendu reproductible."
